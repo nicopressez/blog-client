dc8ae1259643fbbf9d4d853f8017391000a068cd
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-04 20:05:01 +0100

    Added post component logic

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
 const Post = () => {
+    const [post, setPost] = useState([]);
+    const {id} = useParams()
+
+    useEffect(() => {
+        async function getPost() {
+            const response = await fetch(`https://blog-serverapirest.fly.dev/api/posts/${id}`)
+            const obj = await response.json();
+            setPost(obj)
+        }
+        getPost();
+    }, [id])
     return (
-        <div></div>
+        <div>
+            <Link to={"/"}><button>Back to homepage</button></Link>
+            <div className="font-poppins text-center mt-10">
+            <h1 className="text-2xl font-bold">{post.title}</h1>
+            <h2 className="mb-7 mt-2">Written on {post.date}</h2>
+            <p>{post.text}</p>
+            </div>
+        </div>
     )
 }
 
