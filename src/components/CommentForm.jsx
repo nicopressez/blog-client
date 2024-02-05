30ab224d75c9927b34828bb01c9eef0ac39e72cc
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-05 22:11:38 +0100

    Added comment form component & post logic

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,7 +1,38 @@
-const CommentForm = () => {
+import { PropTypes } from "prop-types";
+
+const CommentForm = ({postid}) => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const comment = {
+            username: form.username.target,
+            content: form.content.target,
+        };
+        try {
+            const response = await fetch(`https://blog-serverapirest.fly.dev/api/posts/${postid}/comments`, {
+                method: "POST",
+                body: JSON.stringify(comment)
+            });
+            console.log(await response.json())
+        } catch(err) {
+            console.log(err)
+        }
+    }
     return (
-        <div></div>
+        <div>
+            <h2>New comment</h2>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username(optional):</label>
+            <input name="username" type="text" id="username"></input>
+            <textarea name="content" placeholder="Your comment..."></textarea>
+            <input type="submit" value="Add comment"></input>
+            </form>
+        </div>
     )
+}
+
+CommentForm.propTypes = {
+    postid: PropTypes.string,
 }
 
 export default CommentForm
